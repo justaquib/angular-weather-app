c7c2a71f47faa476ebd4f9467b9d614c2784f99d
JustAquib <techaquib@gmail.com>
2023-07-12 04:39:19 +0530

    Weather app with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weatherapp';
+  wdata:any = {}
+  qdata:any = {}
+
+  private apiUrl = 'https://api.weatherapi.com/v1/forecast.json?key=c666dbf417bf408882e170152231107&q=kolkata';
+  private quoteApiUrl = 'https://api.api-ninjas.com/v1/quotes?category=success';
+  private quoteHeader = {headers: { 'X-Api-Key': 'e2xgZtqCBKqSKmixojSXFw==iUZyMXxrs7mBGR2H'}};
+
+  ngOnInit(): void {
+    initFlowbite();
+
+    axios.get(this.apiUrl)
+    .then(res => {
+      this.wdata = res.data;
+      console.log(res);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+    axios.get(this.quoteApiUrl,this.quoteHeader)
+    .then(res => {
+      this.qdata = res.data[0];
+      console.log(res);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
 }
